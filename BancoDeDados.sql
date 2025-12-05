be9a0715a2ecafa139a44bf7b284b2b0cb65a6a9
Raul Marques Serrano <68698024+raulmarquesserrano@users.noreply.github.com>
2025-12-05 10:47:45 -0300

    Update BancoDeDados.sql
    correção em INSERT, algumas vírgulas e fechamentos de parênteses que causavam erros.

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -2,7 +2,7 @@ CREATE DATABASE db_projeto;
 
 USE db_projeto;
 
-CREATE TABLE tb_funcionarios(
+CREATE TABLE tb_funcionarios (
 	id_funcionario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(100) NOT NULL,
 	profissao VARCHAR(255) NOT NULL,
@@ -13,14 +13,12 @@ CREATE TABLE tb_funcionarios(
 	tempo INT NOT NULL
 );
 
-CREATE TABLE tb_usuarios(
+CREATE TABLE tb_usuarios (
 	id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR (255) NOT NULL,
 	senha VARCHAR (255) NOT NULL,
-	cargo VARCHAR(255) NOT NULL,
+	cargo VARCHAR(255) NOT NULL
 );
 
-INSERT INTO tb_usuarios (nome, senha, cargo) VALUES (
-	(administrador, 123, adm);
-);
-
+INSERT INTO tb_usuarios (nome, senha, cargo) 
+VALUES ( 'administrador', '123', 'adm');
