ba09231916e76ef56f6b5c59cd848a70803e4c70
Raul Marques Serrano <68698024+raulmarquesserrano@users.noreply.github.com>
2025-12-07 02:22:42 -0300

    Update BancoDeDados.sql with phpMyAdmin dump

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -1,24 +1,67 @@
-CREATE DATABASE db_projeto;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07/12/2025 às 06:21
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
-USE db_projeto;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE tb_funcionarios (
-	id_funcionario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(100) NOT NULL,
-	profissao VARCHAR(255) NOT NULL,
-	salario DECIMAL NOT NULL,
-	clt VARCHAR(10) NOT NULL,
-	data_inicio DATETIME NOT NULL,
-	data_final DATETIME NOT NULL,
-	tempo INT NOT NULL
-);
 
-CREATE TABLE tb_usuarios (
-	id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR (255) NOT NULL,
-	senha VARCHAR (255) NOT NULL,
-	cargo VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO tb_usuarios (nome, senha, cargo) 
-VALUES ( 'administrador', '123', 'adm');
+--
+-- Banco de dados: `db_projeto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cargo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_usuarios`
+--
+
+INSERT INTO `tb_usuarios` (`id_usuario`, `nome`, `senha`, `cargo`) VALUES
+(1, 'administrador', '$2y$10$iiz9gfLrzdriChBMbV.Jy.CELzsTGFwkc9yhK6zQavMoSZ1CZcjuu', 'admin');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
