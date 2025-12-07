c558855ce17c4c862abd79d548c1c1e47c0f1a7f
Raul Marques Serrano <68698024+raulmarquesserrano@users.noreply.github.com>
2025-12-07 02:39:09 -0300

    Add `tb_funcionarios` table structure and update time
    Updated generation time and added structure for `tb_funcionarios` table with primary key and auto-increment settings.

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/12/2025 às 06:21
+-- Tempo de geração: 07/12/2025 às 06:37
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `db_projeto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_funcionarios`
+--
+
+CREATE TABLE `tb_funcionarios` (
+  `id_funcionario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `profissao` varchar(255) NOT NULL,
+  `salario` decimal(10,0) NOT NULL,
+  `clt` varchar(10) NOT NULL,
+  `data_inicio` datetime NOT NULL,
+  `data_final` datetime NOT NULL,
+  `tempo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,6 +63,12 @@ INSERT INTO `tb_usuarios` (`id_usuario`, `nome`, `senha`, `cargo`) VALUES
 --
 
 --
+-- Índices de tabela `tb_funcionarios`
+--
+ALTER TABLE `tb_funcionarios`
+  ADD PRIMARY KEY (`id_funcionario`);
+
+--
 -- Índices de tabela `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
@@ -54,6 +77,12 @@ ALTER TABLE `tb_usuarios`
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `tb_funcionarios`
+--
+ALTER TABLE `tb_funcionarios`
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuarios`
